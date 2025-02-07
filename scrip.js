5b3c8d6e093819f4a679c73fcc57a565d1e4627b
siskano <siskanovitaportofolio@gmail.com>
2025-02-07 09:08:16 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,17 @@
+function appendValue(value){
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay(){
+    document.getElementById("display").value = "";
+}
+
+function calculate(){
+    const display = document.getElementById("display");
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Eror";
+    }
+}
+
